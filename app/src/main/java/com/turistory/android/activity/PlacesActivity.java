6b7bb54d90cabb739ef822bb4689a7011766b858
Cristobal Romero <cristobalromerorossi@gmail.com>
2016-11-20 11:35:30 -0500

    Fix launching error.
    modified activities
    -MainActivity.java
    -PlacesActivity.java

--- app/src/main/java/com/turistory/android/activity/PlacesActivity.java
+++ app/src/main/java/com/turistory/android/activity/PlacesActivity.java
@@ -2,6 +2,10 @@ package com.turistory.android.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.turistory.android.activity.view.adapter.RecyclerAdapter;
 
 public class PlacesActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class PlacesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places);
+
+        RecyclerAdapter adapter = new RecyclerAdapter(this);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.MiRecycleView);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
