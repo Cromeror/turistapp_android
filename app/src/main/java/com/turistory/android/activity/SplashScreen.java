01889d9291bf3a17c8b2fc725a1f65b13370da91
Cristóbal Romero Rossi <cristobalromerorossi@gmail.com>
2017-04-09 14:58:43 -0500

    Correcion de vista principal

--- app/src/main/java/com/turistory/android/activity/SplashScreen.java
+++ app/src/main/java/com/turistory/android/activity/SplashScreen.java
@@ -16,13 +16,11 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
-                Intent intent = new Intent(SplashScreen.this, AudioGuideActivity.class);
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(intent);
                 finish();
-
             }
-        },800);
+        }, 800);
     }
 
 }
